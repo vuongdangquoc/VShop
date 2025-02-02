7c639eee936a82983a0dd1207668631267d78218
vuongdangquoc <duytanquang9@gmail.com>
2025-02-02 14:57:46 +0700

    create UI  layout and home page

---
+++ VShop/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VShop.Models.Db;
+
+namespace VShop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly VShopContext _context;
+
+        public ProductsController( VShopContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public List<Product> GetBestSellingItems()
+        {
+
+        }
+    }
+}
