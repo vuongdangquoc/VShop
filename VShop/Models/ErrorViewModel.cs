32b2cfaf63861b4534bb112780b2c0a931295700
vuongdangquoc <duytanquang9@gmail.com>
2025-02-02 13:22:47 +0700

    Add project files.

---
+++ VShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
